4a8f647d824df92cb11143019e5a1a5f7d981c6f
Leopoldo Lira <lira.leopoldo97@gmail.com>
2023-05-22 23:48:14 -0600

    feat(core): swagger added for API documentation

--- movienies/movienies/urls.py
+++ movienies/movienies/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 urlpatterns = [
+    path('', SpectacularSwaggerView.as_view(template_name="swagger-ui.html", url_name="schema"), name="swagger-ui"),
     path('admin/', admin.site.urls),
-    path('authentication/', include('authentication.urls'))
-]
+    path('authentication/', include('authentication.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name="schema"),
+    ]
